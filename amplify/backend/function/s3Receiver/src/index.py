3ba07231ca131b5390b46265a0aa2f39af4c5243
MathiasGilles <mathiasgilles136@gmail.com>
2023-03-17 14:42:49 +0100

    finish exo3

--- amplify/backend/function/s3Receiver/src/index.py
+++ amplify/backend/function/s3Receiver/src/index.py
@@ -1,15 +1,21 @@
 import json
 import os
+import boto3
 import upload
+import requests
 
-bucket = os.environ['STORAGE_BUCKETJSONDATA_BUCKETNAME']
+s3_client = boto3.client("s3")
 
 def handler(event, context):
+  bucket = "bucketjsondata92122-" + os.environ['ENV']
 
   file_key = event['Records'][0]['s3']['object']['key']
-  print(file_key)
-  file = upload.read_file(file_key, bucket)
-  print(file)
+
+  file = json.loads(upload.read_file(file_key, bucket))
+  data = {'data':json.dumps(file['data'])}
+
+  send(file['webhook'], data)
+
   return {
       'statusCode': 200,
       'headers': {
@@ -20,3 +26,11 @@ def handler(event, context):
       'body': json.dumps('Hello from your new Amplify Python lambda!')
   }
 
+def send(url, data):
+  try:
+    requests.post(url, data)
+  except Exception as error:
+    print(error)
+    return False
+
+  return True
